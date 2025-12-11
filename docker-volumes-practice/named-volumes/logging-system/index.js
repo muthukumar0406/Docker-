c57fe363a6c31e74a1d47bc853ab5fabd3807d2a
muthukumar0406 <muthukumarbcs0406@gmail.com>
2025-12-11 09:50:34 +0000

    commit

--- docker-volumes-practice/named-volumes/logging-system/index.js
+++ docker-volumes-practice/named-volumes/logging-system/index.js
@@ -1,7 +1,18 @@
+const express = require("express");
 const fs = require("fs");
+const app = express();
 
-const logFile = "/logs/app.log";
+const logDir = "/namdenodelogs";
+const logFile = `${logDir}/app.log`;
 
-fs.appendFileSync(logFile, `Log saved at: ${new Date()}\n`);
+// Create folder if not exists
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+}
 
-console.log("Log written inside named volume!");
+app.get("/", (req, res) => {
+    fs.appendFileSync(logFile, `Visited at: ${new Date()}\n`);
+    res.send("<h1>Node Named Volume Example</h1><p>Log saved!</p>");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
